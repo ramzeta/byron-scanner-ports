b958671ab57669177f0b132ccaf0f42da8cb7799
ramzeta <ramiroperezsanz13@gmail.com>
2023-02-15 20:45:36 +0100

    update·

--- BRscann.py
+++ BRscann.py
@@ -19,5 +19,6 @@ print("[Info] Herramienta para escanear los puertos abiertos en una dirección I
 import nmap3
 nmap = nmap3.Nmap()
 ip=input("[+] IP Objetivo ==> ")
+# Esto es equivalente a nmap -oX - nmmapper.com -O
 os_results = nmap.nmap_os_detection(ip) # Ejecutar como administrador
 print(os_results)
